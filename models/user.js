db761508a180f9e7f7babd81b02f1b0819383fa3
ghadeerkh9 <ghadeer_khasawneh@yahoo.com>
2022-04-28 01:23:16 +0300

    first commit

---
+++ models/user.js
@@ -0,0 +1,24 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      User.hasMany(models.Movie, {
+        as: "movies",
+        foreignKey: "userId",
+      });
+    }
+  }
+  User.init(
+    {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
